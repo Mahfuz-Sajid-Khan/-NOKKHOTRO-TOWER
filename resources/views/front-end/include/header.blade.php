6c2a8de1f4ed58cd11da9fdb3e07394108276355
Mahfuz Sajid Khan <mahfuzsajid75@gmail.com>
2023-12-23 22:27:55 +0600

    first commit

---
+++ resources/views/front-end/include/header.blade.php
@@ -0,0 +1,24 @@
+<!-- ======= Header ======= -->
+<header id="header" class="d-flex align-items-center">
+    <div class="container d-flex align-items-center justify-content-between">
+
+        <div class="logo">
+            <a href="{{route('home')}}"><img src="{{asset('front-end-assets')}}/img/logo-copy.png" alt="" class="img-fluid"></a>
+        </div>
+
+        <nav id="navbar" class="navbar">
+            <ul>
+                <li><a class="nav-link scrollto active" href="{{route('home')}}">Home</a></li>
+                <li><a class="nav-link scrollto" href="{{route('home')}}#about">About</a></li>
+                <li><a class="nav-link scrollto" href="{{route('home')}}#services">Services</a></li>
+                <li><a class="nav-link scrollto " href="{{route('home')}}#portfolio">Portfolio</a></li>
+                <li><a class="nav-link scrollto" href="{{route('home')}}#team">Team</a></li>
+                <li><a class="nav-link scrollto" href="{{route('home')}}#pricing">Pricing</a></li>
+                <li><a class="nav-link scrollto" href="{{route('home')}}#contact">Contact</a></li>
+            </ul>
+            <i class="bi bi-list mobile-nav-toggle"></i>
+        </nav><!-- .navbar -->
+
+    </div>
+</header>
+<!-- End Header -->
